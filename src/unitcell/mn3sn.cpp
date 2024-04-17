ecf8aa9960044b353470ffb3dbc26a1fb7ea8cca
jacksonross1024 <jacksonross1024@gmail.com>
2024-04-17 14:25:15 +0100

    mn3sn update

--- src/unitcell/mn3sn.cpp
+++ src/unitcell/mn3sn.cpp
@@ -25,9 +25,9 @@ namespace internal{
 void build_mn3sn(unitcell::unit_cell_t& unit_cell){
 
    // rescale y and z unit cell size to preserve unit cell dimensions
-   unit_cell.dimensions[0]=0.5759941877789441;
+   unit_cell.dimensions[0]=0.5746444617738683;
    unit_cell.dimensions[1]=1.0;
-   unit_cell.dimensions[2]=0.4611006474958296;
+   unit_cell.dimensions[2]=0.46002015128929274;
 
    unit_cell.shape[0][0]=1.0;
    unit_cell.shape[0][1]=0.0;
@@ -48,7 +48,7 @@ void build_mn3sn(unitcell::unit_cell_t& unit_cell){
    unit_cell.surface_threshold=3;
    //-----------------------------
    unit_cell.atom[0].x = 0;  
-   unit_cell.atom[0].y = 0.0831376;
+   unit_cell.atom[0].y = 0.08294278;
    unit_cell.atom[0].z = 0.75;
    unit_cell.atom[0].mat = uc::internal::sublattice_materials ? 0 : 0; // if sublattice material is defined, then identify at as same as ID
    unit_cell.atom[0].lc = 0;   // running index 
@@ -72,7 +72,7 @@ void build_mn3sn(unitcell::unit_cell_t& unit_cell){
    unit_cell.atom[2].ni = 20;
    //-----------------------------
    unit_cell.atom[3].x = 0.5;
-   unit_cell.atom[3].y = 0.0831376;
+   unit_cell.atom[3].y = 0.08294278;
    unit_cell.atom[3].z = 0.75;
    unit_cell.atom[3].mat = uc::internal::sublattice_materials ? 3 : 3; // if sublattice material is defined, then identify at as same as ID
    unit_cell.atom[3].lc = 3;
@@ -81,7 +81,7 @@ void build_mn3sn(unitcell::unit_cell_t& unit_cell){
    unit_cell.atom[3].nm = true;
    //-----------------------------
    unit_cell.atom[4].x = 0.5;  // replace by relative coordinates
-   unit_cell.atom[4].y = 0.2494128;
+   unit_cell.atom[4].y = 0.25;
    unit_cell.atom[4].z = 0.25;
    unit_cell.atom[4].mat = uc::internal::sublattice_materials ? 0 : 0; // if sublattice material is defined, then identify at as same as ID
    unit_cell.atom[4].lc = 4;   // running index 
@@ -89,7 +89,7 @@ void build_mn3sn(unitcell::unit_cell_t& unit_cell){
    unit_cell.atom[4].ni = 20;  // (est.) of no. of interactions
    //-----------------------------
    unit_cell.atom[5].x = 0.25;
-   unit_cell.atom[5].y = 0.3337248;
+   unit_cell.atom[5].y = 0.33294278;
    unit_cell.atom[5].z = 0.75;
    unit_cell.atom[5].mat = uc::internal::sublattice_materials ? 2 : 1; // if sublattice material is defined, then identify at as same as ID
    unit_cell.atom[5].lc = 5;
@@ -97,7 +97,7 @@ void build_mn3sn(unitcell::unit_cell_t& unit_cell){
    unit_cell.atom[5].ni = 20;
    //-----------------------------
    unit_cell.atom[6].x = 0.75;
-   unit_cell.atom[6].y = 0.3337248;
+   unit_cell.atom[6].y = 0.33294278;
    unit_cell.atom[6].z = 0.75;
    unit_cell.atom[6].mat = uc::internal::sublattice_materials ? 1 : 2; // if sublattice material is defined, then identify at as same as ID
    unit_cell.atom[6].lc = 6;
@@ -105,7 +105,7 @@ void build_mn3sn(unitcell::unit_cell_t& unit_cell){
    unit_cell.atom[6].ni = 20;
    //-----------------------------
    unit_cell.atom[7].x = 0.0;
-   unit_cell.atom[7].y = 0.2494128;
+   unit_cell.atom[7].y = 0.25;
    unit_cell.atom[7].z = 0.25;
    unit_cell.atom[7].mat = uc::internal::sublattice_materials ? 3 : 3; // if sublattice material is defined, then identify at as same as ID
    unit_cell.atom[7].lc = 7;
@@ -114,7 +114,7 @@ void build_mn3sn(unitcell::unit_cell_t& unit_cell){
    unit_cell.atom[7].nm = true;
       //-----------------------------
    unit_cell.atom[8].x = 0.5;
-   unit_cell.atom[8].y = 0.5831376;
+   unit_cell.atom[8].y = 0.58294278;
    unit_cell.atom[8].z = 0.75;
    unit_cell.atom[8].mat = uc::internal::sublattice_materials ? 0 : 0; // if sublattice material is defined, then identify at as same as ID
    unit_cell.atom[8].lc = 8;
@@ -138,7 +138,7 @@ void build_mn3sn(unitcell::unit_cell_t& unit_cell){
    unit_cell.atom[10].ni = 20;
            //-----------------------------
    unit_cell.atom[11].x = 0.0;
-   unit_cell.atom[11].y = 0.5831376;
+   unit_cell.atom[11].y = 0.58294278;
    unit_cell.atom[11].z = 0.75;
    unit_cell.atom[11].mat = uc::internal::sublattice_materials ? 3 : 3; // if sublattice material is defined, then identify at as same as ID
    unit_cell.atom[11].lc = 11;
@@ -147,7 +147,7 @@ void build_mn3sn(unitcell::unit_cell_t& unit_cell){
    unit_cell.atom[11].nm = true;
               //-----------------------------
    unit_cell.atom[12].x = 0.0;
-   unit_cell.atom[12].y = 0.7494128;
+   unit_cell.atom[12].y = 0.75;
    unit_cell.atom[12].z = 0.25;
    unit_cell.atom[12].mat = uc::internal::sublattice_materials ? 0 : 0; // if sublattice material is defined, then identify at as same as ID
    unit_cell.atom[12].lc = 12;
@@ -155,7 +155,7 @@ void build_mn3sn(unitcell::unit_cell_t& unit_cell){
    unit_cell.atom[12].ni = 20;
                  //-----------------------------
    unit_cell.atom[13].x = 0.75;
-   unit_cell.atom[13].y = 0.8337248;
+   unit_cell.atom[13].y = 0.83294278;
    unit_cell.atom[13].z = 0.75;
    unit_cell.atom[13].mat = uc::internal::sublattice_materials ? 2 : 1; // if sublattice material is defined, then identify at as same as ID
    unit_cell.atom[13].lc = 13;
@@ -163,7 +163,7 @@ void build_mn3sn(unitcell::unit_cell_t& unit_cell){
    unit_cell.atom[13].ni = 20;
                     //-----------------------------
    unit_cell.atom[14].x = 0.25;
-   unit_cell.atom[14].y = 0.8337248;
+   unit_cell.atom[14].y = 0.83294278;
    unit_cell.atom[14].z = 0.75;
    unit_cell.atom[14].mat = uc::internal::sublattice_materials ? 1 : 2; // if sublattice material is defined, then identify at as same as ID
    unit_cell.atom[14].lc = 14;
@@ -171,7 +171,7 @@ void build_mn3sn(unitcell::unit_cell_t& unit_cell){
    unit_cell.atom[14].ni = 20;
                        //-----------------------------
    unit_cell.atom[15].x = 0.5;
-   unit_cell.atom[15].y = 0.7505872;
+   unit_cell.atom[15].y = 0.75;
    unit_cell.atom[15].z = 0.25;
    unit_cell.atom[15].mat = uc::internal::sublattice_materials ? 3 : 3; // if sublattice material is defined, then identify at as same as ID
    unit_cell.atom[15].lc = 15;
