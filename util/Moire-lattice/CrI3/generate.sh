c5b6167419f050447cb1ee96c5c854752a4cb36d
jackson ross <jackross1024@gmail.com>
2024-11-19 17:02:29 +0000

    curie temp updates

--- util/Moire-lattice/CrI3/generate.sh
+++ util/Moire-lattice/CrI3/generate.sh
@@ -1,49 +1,41 @@
 #!/bin/bash
 make
-./main 0.0 9.9 --dmi
-cat header.ucf atom_positions.ucf header_interactions.ucf  interactions.ucf > A-0.0-DMI-3NN/CrI3.ucf
+./main 0.0 9.9 --dmi 0.2 10.0
+cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
+mpirun -np 8 ../../../vampire-parallel
+mv output output-0.2J
 
-./main 0.0 7.7 --dmi
-cat header.ucf atom_positions.ucf header_interactions.ucf  interactions.ucf > A-0.0-DMI-2NN/CrI3.ucf
+./main 0.0 9.9 --dmi 0.225 10.0
+cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
+mpirun -np 8 ../../../vampire-parallel
+mv output output-0.225J
 
-./main 0.0 9.9 
-cat header.ucf atom_positions.ucf header_interactions.ucf  interactions.ucf > A-0.0-noDMI-3NN/CrI3.ucf
+./main 0.0 9.9 --dmi 0.25 10.0
+cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
+mpirun -np 8 ../../../vampire-parallel
+mv output output-0.25J
 
-./main 0.0 7.7
-cat header.ucf atom_positions.ucf header_interactions.ucf  interactions.ucf > A-0.0-noDMI-2NN/CrI3.ucf
+./main 0.0 9.9 --dmi 0.275 10.0
+cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
+mpirun -np 8 ../../../vampire-parallel
+mv output output-0.275J
 
-./main 0.5 9.9 --dmi
-cat header.ucf atom_positions.ucf header_interactions.ucf  interactions.ucf > A-0.5-DMI-3NN/CrI3.ucf
+./main 0.5 9.9 --dmi 0.2 10.0
+cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
+mpirun -np 8 ../../../vampire-parallel
+mv output output-0.2J-0.5
 
-./main 0.5 7.7 --dmi
-cat header.ucf atom_positions.ucf header_interactions.ucf  interactions.ucf > A-0.5-DMI-2NN/CrI3.ucf
+./main 0.5 9.9 --dmi 0.225 10.0
+cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
+mpirun -np 8 ../../../vampire-parallel
+mv output output-0.225J-0.5
 
-./main 0.5 9.9 
-cat header.ucf atom_positions.ucf header_interactions.ucf  interactions.ucf > A-0.5-noDMI-3NN/CrI3.ucf
+./main 0.5 9.9 --dmi 0.25 10.0
+cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
+mpirun -np 8 ../../../vampire-parallel
+mv output output-0.25J-0.5
 
-./main 0.5 7.7
-cat header.ucf atom_positions.ucf header_interactions.ucf  interactions.ucf > A-0.5-noDMI-2NN/CrI3.ucf
-
-./main 1.1 9.9 --dmi
-cat header.ucf atom_positions.ucf header_interactions.ucf  interactions.ucf > A-1.1-DMI-3NN/CrI3.ucf
-
-./main 1.1 7.7 --dmi
-cat header.ucf atom_positions.ucf header_interactions.ucf  interactions.ucf > A-1.1-DMI-2NN/CrI3.ucf
-
-./main 1.1 9.9 
-cat header.ucf atom_positions.ucf header_interactions.ucf  interactions.ucf > A-1.1-noDMI-3NN/CrI3.ucf
-
-./main 1.1 7.7
-cat header.ucf atom_positions.ucf header_interactions.ucf  interactions.ucf > A-1.1-noDMI-2NN/CrI3.ucf
-
-./main 1.41 9.9 --dmi
-cat header.ucf atom_positions.ucf header_interactions.ucf  interactions.ucf > A-1.41-DMI-3NN/CrI3.ucf
-
-./main 1.41 7.7 --dmi
-cat header.ucf atom_positions.ucf header_interactions.ucf  interactions.ucf > A-1.41-DMI-2NN/CrI3.ucf
-
-./main 1.41 9.9 
-cat header.ucf atom_positions.ucf header_interactions.ucf  interactions.ucf > A-1.41-noDMI-3NN/CrI3.ucf
-
-./main 1.41 7.7
-cat header.ucf atom_positions.ucf header_interactions.ucf  interactions.ucf > A-1.41-noDMI-2NN/CrI3.ucf
+./main 0.5 9.9 --dmi 0.275 10.0
+cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
+mpirun -np 8 ../../../vampire-parallel
+mv output output-0.275J-0.5
