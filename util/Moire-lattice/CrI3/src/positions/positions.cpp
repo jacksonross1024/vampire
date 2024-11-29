64bb39540dfdf40f31841d518aa08b101918d4a0
jacksonross1024 <jacksonross1024@gmail.com>
2024-11-29 16:59:28 +0000

    no dipole energy

--- util/Moire-lattice/CrI3/src/positions/positions.cpp
+++ util/Moire-lattice/CrI3/src/positions/positions.cpp
@@ -127,8 +127,8 @@ void create_magnetic_atom_list(std::string filename){
                         // int changey = round(-100.0*remainder(y_new - y_j,a1y)/a0x)+100;
                         double x_eff = x_j*cos(twist_angle) - y_j*sin(twist_angle);
                         double y_eff = y_j*cos(twist_angle) + x_j*sin(twist_angle);
-                        double x_ref = (i*a0x + j*a1x + 4.62)*cos(twist_angle)-(((new_atom.l_id == 1) ? (4.001) : (0.0)) + j*a1y)*sin(twist_angle); 
-                        double y_ref = (i*a0x + j*a1x + 4.62)*sin(twist_angle)+(((new_atom.l_id == 1) ? (4.001) : (0.0)) + j*a1y)*cos(twist_angle); 
+                        double x_ref = (i*a0x + j*a1x + 4.62)*cos(twist_angle)-(((new_atom.l_id == 1) ? (4.00) : (0.0)) + j*a1y)*sin(twist_angle); 
+                        double y_ref = (i*a0x + j*a1x + 4.62)*sin(twist_angle)+(((new_atom.l_id == 1) ? (4.00) : (0.0)) + j*a1y)*cos(twist_angle); 
                         
                         int changey = int(round(10*(fmod(std::abs(y_eff-y_ref) , a1y)/a1y)));
                         int changex = int(round(9*(fmod(std::abs(x_eff-x_ref +changey*a1y/11.0) , a0x)/a0x)));
