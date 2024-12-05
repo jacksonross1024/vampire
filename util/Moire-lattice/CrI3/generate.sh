fcf401a278b62f6372e86908ee6063f1c8749d8f
jacksonross1024 <jacksonross1024@gmail.com>
2024-12-05 07:28:47 +0000

    gen

--- util/Moire-lattice/CrI3/generate.sh
+++ util/Moire-lattice/CrI3/generate.sh
@@ -1,41 +1,244 @@
 #!/bin/bash
-make
-./main 0.0 9.9 --dmi 0.2 10.0
+# make
+./main 1.1 9.9 --dmi 0.25 10.0
 cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
-mpirun -np 8 ../../../vampire-parallel
-mv output output-0.2J
+cat input-mc > input
+mpirun -np 16 ../../../vampire-parallel
+cat input-dipole > input
+mpirun -np 16 ../../../vampire-parallel
 
-./main 0.0 9.9 --dmi 0.225 10.0
-cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
-mpirun -np 8 ../../../vampire-parallel
-mv output output-0.225J
+../../vdc/vdc --cells --cell-size = 10,10,30
+mv cells-00000000.txt cells-1.1-5K-0.25-10-FC.txt
+mv cells-00000001.txt cells-1.1-5K-0.25-10-AF.txt
 
-./main 0.0 9.9 --dmi 0.25 10.0
-cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
-mpirun -np 8 ../../../vampire-parallel
-mv output output-0.25J
+../../vdc/vdc --cells --cell-size = 100,100,30
+mv cells-00000000.txt cells-lowres-1.1-5K-0.25-10-FC.txt
+mv cells-00000001.txt cells-lowres-1.1-5K-0.25-10-AF.txt
+paste cells-coords.cfg cells-00000000.cfg > dipole-cells-1.1-5K-0.25-10-AF.data
 
-./main 0.0 9.9 --dmi 0.275 10.0
-cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
-mpirun -np 8 ../../../vampire-parallel
-mv output output-0.275J
 
-./main 0.5 9.9 --dmi 0.2 10.0
+./main 1.1 9.9 --dmi 0.25 7.0
 cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
-mpirun -np 8 ../../../vampire-parallel
-mv output output-0.2J-0.5
+cat input-mc > input
+mpirun -np 16 ../../../vampire-parallel
+cat input-dipole > input
+mpirun -np 16 ../../../vampire-parallel
 
-./main 0.5 9.9 --dmi 0.225 10.0
+../../vdc/vdc --cells --cell-size = 10,10,30
+mv cells-00000000.txt cells-1.1-5K-0.25-7-FC.txt
+mv cells-00000001.txt cells-1.1-5K-0.25-7-AF.txt
+../../vdc/vdc --cells --cell-size = 100,100,30
+mv cells-00000000.txt cells-lowres-1.1-5K-0.25-7-FC.txt
+mv cells-00000001.txt cells-lowres-1.1-5K-0.25-7-AF.txt
+paste cells-coords.cfg cells-00000000.cfg > dipole-cells-1.1-5K-0.25-7-AF.data
+
+
+./main 1.1 9.9 --dmi 0.25 4.0
 cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
-mpirun -np 8 ../../../vampire-parallel
-mv output output-0.225J-0.5
+cat input-mc > input
+mpirun -np 16 ../../../vampire-parallel
+cat input-dipole > input
+mpirun -np 16 ../../../vampire-parallel
+
+../../vdc/vdc --cells --cell-size = 10,10,30
+mv cells-00000000.txt cells-1.1-5K-0.25-4-FC.txt
+mv cells-00000001.txt cells-1.1-5K-0.25-4-AF.txt
+../../vdc/vdc --cells --cell-size = 100,100,30
+mv cells-00000000.txt cells-lowres-1.1-5K-0.25-4-FC.txt
+mv cells-00000001.txt cells-lowres-1.1-5K-0.25-4-AF.txt
+paste cells-coords.cfg cells-00000000.cfg > dipole-cells-1.1-5K-0.25-4-AF.data
+
+
+./main 1.1 9.9 --dmi 0.30 10.0
+cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
+cat input-mc > input
+mpirun -np 16 ../../../vampire-parallel
+cat input-dipole > input
+mpirun -np 16 ../../../vampire-parallel
+
+../../vdc/vdc --cells --cell-size = 10,10,30
+mv cells-00000000.txt cells-1.1-5K-0.3-10-FC.txt
+mv cells-00000001.txt cells-1.1-5K-0.3-10-AF.txt
+../../vdc/vdc --cells --cell-size = 100,100,30
+mv cells-00000000.txt cells-lowres-1.1-5K-0.3-10-FC.txt
+mv cells-00000001.txt cells-lowres-1.1-5K-0.3-10-AF.txt
+paste cells-coords.cfg cells-00000000.cfg > dipole-cells-1.1-5K-0.3-10-AF.data
+
+
+./main 1.1 9.9 --dmi 0.3 7.0
+cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
+cat input-mc > input
+mpirun -np 16 ../../../vampire-parallel
+cat input-dipole > input
+mpirun -np 16 ../../../vampire-parallel
+
+../../vdc/vdc --cells --cell-size = 10,10,30
+mv cells-00000000.txt cells-1.1-5K-0.3-7-FC.txt
+mv cells-00000001.txt cells-1.1-5K-0.3-7-AF.txt
+../../vdc/vdc --cells --cell-size = 100,100,30
+mv cells-00000000.txt cells-lowres-1.1-5K-0.3-7-FC.txt
+mv cells-00000001.txt cells-lowres-1.1-5K-0.3-7-AF.txt
+paste cells-coords.cfg cells-00000000.cfg > dipole-cells-1.1-5K-0.3-7-AF.data
+
+./main 1.1 9.9 --dmi 0.3 4.0
+cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
+cat input-mc > input
+mpirun -np 16 ../../../vampire-parallel
+cat input-dipole > input
+mpirun -np 16 ../../../vampire-parallel
+
+../../vdc/vdc --cells --cell-size = 10,10,30
+mv cells-00000000.txt cells-1.1-5K-0.3-4-FC.txt
+mv cells-00000001.txt cells-1.1-5K-0.3-4-AF.txt
+../../vdc/vdc --cells --cell-size = 100,100,30
+mv cells-00000000.txt cells-lowres-1.1-5K-0.3-4-FC.txt
+mv cells-00000001.txt cells-lowres-1.1-5K-0.3-4-AF.txt
+paste cells-coords.cfg cells-00000000.cfg > dipole-cells-1.1-5K-0.3-4-AF.data
+
+
+./main 1.1 9.9 --dmi 0.2 10.0
+cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
+cat input-mc > input
+mpirun -np 16 ../../../vampire-parallel
+cat input-dipole > input
+mpirun -np 16 ../../../vampire-parallel
+
+../../vdc/vdc --cells --cell-size = 10,10,30
+mv cells-00000000.txt cells-1.1-5K-0.2-10-FC.txt
+mv cells-00000001.txt cells-1.1-5K-0.2-10-AF.txt
+../../vdc/vdc --cells --cell-size = 100,100,30
+mv cells-00000000.txt cells-1.1-lowres-5K-0.2-10-FC.txt
+mv cells-00000001.txt cells-1.1-lowres-5K-0.2-10-AF.txt
+paste cells-coords.cfg cells-00000000.cfg > dipole-cells-1.1-5K-0.2-10-AF.data
+
+
+./main 1.1 9.9 --dmi 0.2 7.0
+cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
+cat input-mc > input
+mpirun -np 16 ../../../vampire-parallel
+cat input-dipole > input
+mpirun -np 16 ../../../vampire-parallel
+
+../../vdc/vdc --cells --cell-size = 10,10,30
+mv cells-00000000.txt cells-1.1-5K-0.2-7-FC.txt
+mv cells-00000001.txt cells-1.1-5K-0.2-7-AF.txt
+../../vdc/vdc --cells --cell-size = 100,100,30
+mv cells-00000000.txt cells-lowres-1.1-5K-0.2-7-FC.txt
+mv cells-00000001.txt cells-lowres-1.1-5K-0.2-7-AF.txt
+paste cells-coords.cfg cells-00000000.cfg > dipole-cells-1.1-5K-0.2-7-AF.data
+
+
+./main 1.1 9.9 --dmi 0.2 4.0
+cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
+cat input-mc > input
+mpirun -np 16 ../../../vampire-parallel
+cat input-dipole > input
+mpirun -np 16 ../../../vampire-parallel
+
+../../vdc/vdc --cells --cell-size = 10,10,30
+mv cells-00000000.txt cells-1.1-5K-0.2-4-FC.txt
+mv cells-00000001.txt cells-1.1-5K-0.2-4-AF.txt
+../../vdc/vdc --cells --cell-size = 100,100,30
+mv cells-00000000.txt cells-lowres-1.1-5K-0.2-4-FC.txt
+mv cells-00000001.txt cells-lowres-1.1-5K-0.2-4-AF.txt
+paste cells-coords.cfg cells-00000000.cfg > dipole-cells-1.1-5K-0.2-4-AF.data
+
+
+./main 2.0 9.9 --dmi 0.25 10.0
+cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
+cat input-mc > input
+mpirun -np 16 ../../../vampire-parallel
+cat input-dipole > input
+mpirun -np 16 ../../../vampire-parallel
+
+../../vdc/vdc --cells --cell-size = 10,10,30
+mv cells-00000000.txt cells-2.0-5K-0.25-10-FC.txt
+mv cells-00000001.txt cells-2.0-5K-0.25-10-AF.txt
+../../vdc/vdc --cells --cell-size = 100,100,30
+mv cells-00000000.txt cells-lowres-2.0-5K-0.25-10-FC.txt
+mv cells-00000001.txt cells-lowres-2.0-5K-0.25-10-AF.txt
+paste cells-coords.cfg cells-00000000.cfg > dipole-cells-2.0-5K-0.25-10-AF.data
+
+
+./main 2.0 9.9 --dmi 0.25 7.0
+cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
+cat input-mc > input
+mpirun -np 16 ../../../vampire-parallel
+cat input-dipole > input
+mpirun -np 16 ../../../vampire-parallel
+
+../../vdc/vdc --cells --cell-size = 10,10,30
+mv cells-00000000.txt cells-2.0-5K-0.25-7-FC.txt
+mv cells-00000001.txt cells-2.0-5K-0.25-7-AF.txt
+../../vdc/vdc --cells --cell-size = 100,100,30
+mv cells-00000000.txt cells-lowres-2.0-5K-0.25-7-FC.txt
+mv cells-00000001.txt cells-lowres-2.0-5K-0.25-7-AF.txt
+paste cells-coords.cfg cells-00000000.cfg > dipole-cells-2.0-5K-0.25-7-AF.data
+
+
+./main 2.0 9.9 --dmi 0.25 4.0
+cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
+cat input-mc > input
+mpirun -np 16 ../../../vampire-parallel
+cat input-dipole > input
+mpirun -np 16 ../../../vampire-parallel
+
+../../vdc/vdc --cells --cell-size = 10,10,30
+mv cells-00000000.txt cells-2.0-5K-0.25-4-FC.txt
+mv cells-00000001.txt cells-2.0-5K-0.25-4-AF.txt
+../../vdc/vdc --cells --cell-size = 100,100,30
+mv cells-00000000.txt cells-lowres-2.0-5K-0.25-4-FC.txt
+mv cells-00000001.txt cells-lowres-2.0-5K-0.25-4-AF.txt
+paste cells-coords.cfg cells-00000000.cfg > dipole-cells-2.0-5K-0.25-4-AF.data
+
+
 
 ./main 0.5 9.9 --dmi 0.25 10.0
 cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
-mpirun -np 8 ../../../vampire-parallel
-mv output output-0.25J-0.5
+cat input-mc > input
+mpirun -np 16 ../../../vampire-parallel
+cat input-dipole > input
+mpirun -np 16 ../../../vampire-parallel
 
-./main 0.5 9.9 --dmi 0.275 10.0
+../../vdc/vdc --cells --cell-size = 10,10,30
+mv cells-00000000.txt cells-0.5-5K-0.25-10-FC.txt
+mv cells-00000001.txt cells-0.5-5K-0.25-10-AF.txt
+
+../../vdc/vdc --cells --cell-size = 100,100,30
+mv cells-00000000.txt cells-lowres-0.5-5K-0.25-10-FC.txt
+mv cells-00000001.txt cells-lowres-0.5-5K-0.25-10-AF.txt
+paste cells-coords.cfg cells-00000000.cfg > dipole-cells-0.5-5K-0.25-10-AF.data
+
+
+./main 0.5 9.9 --dmi 0.25 7.0
 cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
-mpirun -np 8 ../../../vampire-parallel
-mv output output-0.275J-0.5
+cat input-mc > input
+mpirun -np 16 ../../../vampire-parallel
+cat input-dipole > input
+mpirun -np 16 ../../../vampire-parallel
+
+../../vdc/vdc --cells --cell-size = 10,10,30
+mv cells-00000000.txt cells-0.5-5K-0.25-7-FC.txt
+mv cells-00000001.txt cells-0.5-5K-0.25-7-AF.txt
+../../vdc/vdc --cells --cell-size = 100,100,30
+mv cells-00000000.txt cells-lowres-0.5-5K-0.25-7-FC.txt
+mv cells-00000001.txt cells-lowres-0.5-5K-0.25-7-AF.txt
+paste cells-coords.cfg cells-00000000.cfg > dipole-cells-0.5-5K-0.25-7-AF.data
+
+
+./main 0.5 9.9 --dmi 0.25 4.0
+cat header.ucf atom_positions.xyz header_interactions.ucf  interactions.ucf > CrI3.ucf
+cat input-mc > input
+mpirun -np 16 ../../../vampire-parallel
+cat input-dipole > input
+mpirun -np 16 ../../../vampire-parallel
+
+../../vdc/vdc --cells --cell-size = 10,10,30
+mv cells-00000000.txt cells-0.5-5K-0.25-4-FC.txt
+mv cells-00000001.txt cells-0.5-5K-0.25-4-AF.txt
+../../vdc/vdc --cells --cell-size = 100,100,30
+mv cells-00000000.txt cells-lowres-0.5-5K-0.25-4-FC.txt
+mv cells-00000001.txt cells-lowres-0.5-5K-0.25-4-AF.txt
+paste cells-coords.cfg cells-00000000.cfg > dipole-cells-0.5-5K-0.25-4-AF.data
+
+
