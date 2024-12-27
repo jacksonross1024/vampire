1f9e7bb066fe48d64cc42e77dfedf6f5df67cc42
jackson ross <jackross1024@gmail.com>
2024-12-27 13:39:30 +0000

    better vmpr.sh file

--- util/Moire-lattice/CrI3/vmpr.sh
+++ util/Moire-lattice/CrI3/vmpr.sh
@@ -1,27 +1,40 @@
-
 #!/bin/bash
 
 # Slurm job options (name, compute nodes, job time)
 #SBATCH --job-name=CrI3-dipole
-#SBATCH --time=1:00:00
+#SBATCH --time=6:00:00
 #SBATCH --nodes=1
-#SBATCH --tasks-per-node=32
+#SBATCH --tasks-per-node=28
 #SBATCH --cpus-per-task=1
 
 #SBATCH --account=e89-ed_p
 #SBATCH --partition=standard
-#SBATCH --qos=standard
+#SBATCH --qos=taskfarm
+
 module load PrgEnv-gnu
 export OMP_NUM_THREADS=1
 
-mc > input
-
+input-mc-20K > input
 srun --distribution=block:cyclic:block /work/e89/e89/jross71/vampire/moire/vampire-parallel
-../../vdc/vdc --cells --cell-size 10,10,30
+../../../../vdc/vdc --cells --cell-size 10,10,30
+mv cells-00000000.txt cells-20K-AF.txt
+mv cells-00000001.txt cells-20K-FC.txt
 
-cat input-dipole > input
+
+cat input-dipole-20K > input
 srun --distribution=block:cyclic:block /work/e89/e89/jross71/vampire/moire/vampire-parallel
 
-paste cells-coords.cfg cells-00000000.cfg > dipole-cells-AF.data
-rm CrI3.ucf
+paste cells-coords.cfg cells-00000000.cfg > dipole-cells-20K-AF.data
+
+input-mc-5K > input
+srun --distribution=block:cyclic:block /work/e89/e89/jross71/vampire/moire/vampire-parallel
+../../../../vdc/vdc --cells --cell-size 10,10,30
+mv cells-00000000.txt cells-5K-AF.txt
+mv cells-00000001.txt cells-5K-FC.txt
+
+
+cat input-dipole-5K > input
+srun --distribution=block:cyclic:block /work/e89/e89/jross71/vampire/moire/vampire-parallel
+
+paste cells-coords.cfg cells-00000000.cfg > dipole-cells-5K-AF.data
 
