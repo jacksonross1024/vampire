dd61361d418ce20447a64950639bcff83893525d
jacksonross1024 <jacksonross1024@gmail.com>
2025-03-25 15:02:42 +0000

    moire pbc, clutser, 0.5 only

--- util/Moire-lattice/CrI3-pbc/src/main.cpp
+++ util/Moire-lattice/CrI3-pbc/src/main.cpp
@@ -66,7 +66,7 @@ int main(int argc, char* argv[]){
 
     twist_loction = 2*system_size_z/5 -0.01;
     std::cout << "twisting at: " << twist_loction << std::endl;
-    read_in_atoms("files/atom_list_abprimebprimea_rhombic", num_atoms, atom);
+    read_in_atoms("../../files/atom_list_abprimebprimea_rhombic", num_atoms, atom);
     // read_in_atoms("files/atom_list_aa_rhombic", num_ref_atoms, ref_atom);
    //  read_in_dft("files/criteria.txt");
     //  read_in_atoms("files/nm_atoms", num_nm_atoms, nm_atom);
@@ -75,46 +75,46 @@ int main(int argc, char* argv[]){
     {  
         #pragma omp section
         {
-            read_in_intra_exchanges("bilayer_sliding/Cr1_intra_data.txt", Eintra_Cr1_1NN, Eintra_Cr1_2NN, Eintra_Cr1_3NN);
-               read_in_inter_exchanges("bilayer_sliding/Cr1_inter_map_2.txt",\
-                               "bilayer_sliding/Cr1_Dx_inter_map_2_avg.txt",\
-                               "bilayer_sliding/Cr1_Dy_inter_map_2_avg.txt",\
-                               "bilayer_sliding/Cr1_Dz_inter_map_2_avg.txt", Einter_Cr1);
+            read_in_intra_exchanges("../../bilayer_sliding/Cr1_intra_data.txt", Eintra_Cr1_1NN, Eintra_Cr1_2NN, Eintra_Cr1_3NN);
+               read_in_inter_exchanges("../../bilayer_sliding/Cr1_inter_map_2.txt",\
+                               "../../bilayer_sliding/Cr1_Dx_inter_map_2_avg.txt",\
+                               "../../bilayer_sliding/Cr1_Dy_inter_map_2_avg.txt",\
+                               "../../bilayer_sliding/Cr1_Dz_inter_map_2_avg.txt", Einter_Cr1);
         }
         #pragma omp section
         {
-            read_in_intra_exchanges("bilayer_sliding/Cr2_intra_data.txt", Eintra_Cr2_1NN, Eintra_Cr2_2NN, Eintra_Cr2_3NN);
-                read_in_inter_exchanges("bilayer_sliding/Cr2_inter_map_2.txt",\
-                               "bilayer_sliding/Cr2_Dx_inter_map_2_avg.txt",\
-                               "bilayer_sliding/Cr2_Dy_inter_map_2_avg.txt",\
-                               "bilayer_sliding/Cr2_Dz_inter_map_2_avg.txt", Einter_Cr2);
+            read_in_intra_exchanges("../../bilayer_sliding/Cr2_intra_data.txt", Eintra_Cr2_1NN, Eintra_Cr2_2NN, Eintra_Cr2_3NN);
+                read_in_inter_exchanges("../../bilayer_sliding/Cr2_inter_map_2.txt",\
+                               "../../bilayer_sliding/Cr2_Dx_inter_map_2_avg.txt",\
+                               "../../bilayer_sliding/Cr2_Dy_inter_map_2_avg.txt",\
+                               "../../bilayer_sliding/Cr2_Dz_inter_map_2_avg.txt", Einter_Cr2);
         }
         #pragma omp section
         {
-            read_in_intra_exchanges("bilayer_sliding/Cr3_intra_data.txt", Eintra_Cr3_1NN, Eintra_Cr3_2NN, Eintra_Cr3_3NN);
-                read_in_inter_exchanges("bilayer_sliding/Cr3_inter_map_2.txt",\
-                               "bilayer_sliding/Cr3_Dx_inter_map_2_avg.txt",\
-                               "bilayer_sliding/Cr3_Dy_inter_map_2_avg.txt",\
-                               "bilayer_sliding/Cr3_Dz_inter_map_2_avg.txt", Einter_Cr3);
+            read_in_intra_exchanges("../../bilayer_sliding/Cr3_intra_data.txt", Eintra_Cr3_1NN, Eintra_Cr3_2NN, Eintra_Cr3_3NN);
+                read_in_inter_exchanges("../../bilayer_sliding/Cr3_inter_map_2.txt",\
+                               "../../bilayer_sliding/Cr3_Dx_inter_map_2_avg.txt",\
+                               "../../bilayer_sliding/Cr3_Dy_inter_map_2_avg.txt",\
+                               "../../bilayer_sliding/Cr3_Dz_inter_map_2_avg.txt", Einter_Cr3);
         }
         #pragma omp section
         {
-            read_in_intra_exchanges("bilayer_sliding/Cr4_intra_data.txt", Eintra_Cr4_1NN, Eintra_Cr4_2NN, Eintra_Cr4_3NN);
-                read_in_inter_exchanges("bilayer_sliding/Cr4_inter_map_2.txt",\
-                               "bilayer_sliding/Cr4_Dx_inter_map_2_avg.txt",\
-                               "bilayer_sliding/Cr4_Dy_inter_map_2_avg.txt",\
-                               "bilayer_sliding/Cr4_Dz_inter_map_2_avg.txt", Einter_Cr4);
+            read_in_intra_exchanges("../../bilayer_sliding/Cr4_intra_data.txt", Eintra_Cr4_1NN, Eintra_Cr4_2NN, Eintra_Cr4_3NN);
+                read_in_inter_exchanges("../../bilayer_sliding/Cr4_inter_map_2.txt",\
+                               "../../bilayer_sliding/Cr4_Dx_inter_map_2_avg.txt",\
+                               "../../bilayer_sliding/Cr4_Dy_inter_map_2_avg.txt",\
+                               "../../bilayer_sliding/Cr4_Dz_inter_map_2_avg.txt", Einter_Cr4);
         }
     }
 
 /*        //     }
-// //    read_in_inter_exchanges("bilayer_sliding/Cr4_inter.txt", Einter_Cr4);
+// //    read_in_inter_exchanges("../../bilayer_sliding/Cr4_inter.txt", Einter_Cr4);
 //     // exit(1);
-//    // read_in_intra_exchanges("bilayer_sliding/Cr1_intra.txt", Eintra_Cr1_1NN, Eintra_Cr1_2NN, Eintra_Cr1_3NN);
-//    // read_in_intra_exchanges("bilayer_sliding/Cr2_intra.txt", Eintra_Cr2_1NN, Eintra_Cr2_2NN, Eintra_Cr2_3NN);
-//    // read_in_intra_exchanges("bilayer_sliding/Cr3_intra.txt", Eintra_Cr3_1NN, Eintra_Cr3_2NN, Eintra_Cr3_3NN);
-//    // read_in_intra_exchanges("bilayer_sliding/Cr4_intra.txt", Eintra_Cr4_1NN, Eintra_Cr4_2NN, Eintra_Cr4_3NN);
-//    std::ifstream ifile1("bilayer_sliding/Cr1_intra.txt");
+//    // read_in_intra_exchanges("../../bilayer_sliding/Cr1_intra.txt", Eintra_Cr1_1NN, Eintra_Cr1_2NN, Eintra_Cr1_3NN);
+//    // read_in_intra_exchanges("../../bilayer_sliding/Cr2_intra.txt", Eintra_Cr2_1NN, Eintra_Cr2_2NN, Eintra_Cr2_3NN);
+//    // read_in_intra_exchanges("../../bilayer_sliding/Cr3_intra.txt", Eintra_Cr3_1NN, Eintra_Cr3_2NN, Eintra_Cr3_3NN);
+//    // read_in_intra_exchanges("../../bilayer_sliding/Cr4_intra.txt", Eintra_Cr4_1NN, Eintra_Cr4_2NN, Eintra_Cr4_3NN);
+//    std::ifstream ifile1("../../bilayer_sliding/Cr1_intra.txt");
 //    std::string line;
 //     if(!ifile1.is_open()) {std::cerr  << " is not open" << std::endl; exit(1);}
 //    for(int i=0; i<Eintra_Cr1_1NN.size()*11; i++){
