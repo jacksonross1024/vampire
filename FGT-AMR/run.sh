9081f026a5a9cde8fdffcf0d8cd36fdb58efb965
jackson ross <jackross1024@gmail.com>
2025-08-19 10:33:48 +0100

    amr inputs

--- FGT-AMR/run.sh
+++ FGT-AMR/run.sh
@@ -1,15 +1,20 @@
 
 
 mkdir -p spin-resistance 
+mkdir FC-chk
+
 
 cat input-FC > input
 mpirun -np 16 ../vampire-parallel
 ../util/vdc/vdc --cells --cell-size = 10,10,21
 cat cells-00000000.txt > cells-FC.txt
+cp *chk FC-chk/
+
 
 cat input-SOT > input
 mpirun -np 16 ../vampire-parallel
 ../util/vdc/vdc --cells --cell-size = 10,10,21
+
 
 cat input-amr > input
 mpirun -np 16 ../vampire-parallel
